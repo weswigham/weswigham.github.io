bb9e3fac57961f146049f977c75b0e7406503bcd
weswigham <wwigham@gmail.com>
2014-01-18 09:45:20 -0700

    fresh start, going to be less rails

--- README.md
+++
@@ -1,4 +0,0 @@
-simplyme
-========
-
-A rails application designed for use as my personal website.
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Simplyme::Application.config.secret_token = '6848c32c4d1060faefb149ea75f7252b37ade652afd1da0be1c08851f2cb14f117b8aca3ad79bc05b05be800eb5c83f259b8ac722533fc3da094b2f64d000dd1'
